609b4f6587332177ea3f4ac7dbf4fa10869eb75a
Sean <seantsusmilch@gmail.com>
2021-08-30 12:44:35 -0500

    🧼Cleanup

--- src/components/Auth/UserProvider.tsx
+++ src/components/Auth/UserProvider.tsx
@@ -11,7 +11,6 @@ export const UserContext = createContext(auth.currentUser as any)
 
 export interface User {
     uid: string
-    
 }
 
 export let idToken = ''
@@ -24,38 +23,6 @@ export const UserProvider: React.FC = (props)=>{
 
     useEffect(() => {
         auth.onAuthStateChanged(async (usr:any)=>{
-            
-            /** @todo
-             * TODO:
-             * Go to the backend and get user data
-             * Add userdata to this usr object
-             */
-
-            // usr?.getIdToken(true).then((token:string)=>{
-            //     // console.log('idToken ',token)
-            //     let uid = usr?.uid
-            //     axios({
-            //         method: 'POST',
-            //         url: `${backend}/user/${uid}`,
-            //         data:{
-            //             idToken: token
-            //         }
-            //     })
-            //         .then((res)=>{
-            //             // console.log('resdata',usr)
-            //             usr = {
-            //                 ...usr,
-            //                 ...res.data.user
-            //             }
-            //             setUser(usr)
-            //             idToken = token
-            //             // console.log('Logged in?')
-            //             return
-            //         })
-            //     // axios.post(`${backend}/user/${uid}`,{idToken: token})
-            // })
-
-            // console.log(usr.uid)
 
             if(!usr){
                 console.log('No user')
@@ -79,12 +46,6 @@ export const UserProvider: React.FC = (props)=>{
             // localStorage.setItem('user', JSON.stringify(usr))
         })
     },[])
-
-    // console.log('current user ', JSON.stringify(auth.currentUser))
-    // if(!auth.currentUser){
-    //     signInAnon()
-    //     console.log(auth.currentUser)
-    // }
 
     return (<>
         {loaded?
